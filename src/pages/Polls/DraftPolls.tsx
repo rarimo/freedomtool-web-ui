b2cfeea57becdaffffa0220a4d9587d1e7d58a8e
ardier16 <ardier16@gmail.com>
2025-04-09 16:17:13 +0300

    Remove draft polls

--- src/pages/Polls/DraftPolls.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack, Typography } from '@mui/material'
-
-export default function DraftPolls() {
-  return (
-    <Stack>
-      <Typography variant='subtitle1'>Coming Soon!</Typography>
-    </Stack>
-  )
-}
