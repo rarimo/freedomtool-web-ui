09b5758f6baccf3c08a004e35fb8233b2d71b68e
ardier16 <ardier16@gmail.com>
2025-08-07 11:41:56 +0300

    Update donation addresses

--- src/constants/donation-token-config.ts
+++ src/constants/donation-token-config.ts
@@ -11,19 +11,19 @@ export const donationTokens: DonationToken[] = [
   {
     name: 'BTC (Bitcoin)',
     symbol: 'BTC',
-    address: 'bc1qzqwnvcmxd7n3fpa7mqcvlnprrtcp5hrt649ut7',
+    address: 'bc1q4u7ecz9hlk6exc22v5hz62l54w7g0fvl0d2yh6',
     iconName: Icons.Bitcoin,
   },
   {
     name: 'ETH (Ethereum)',
     symbol: 'ETH',
-    address: '0x9Dc54bB3Ea2EBdACA4c6f86647FBE09FFaE3BCee',
+    address: '0x97Aff1180f0A307e7456e65b29a3A6D7D71f5140',
     iconName: Icons.Ethereum,
   },
   {
     name: 'USDC (ERC-20)',
     symbol: 'USDC',
-    address: '0x9Dc54bB3Ea2EBdACA4c6f86647FBE09FFaE3BCee',
+    address: '0x97Aff1180f0A307e7456e65b29a3A6D7D71f5140',
     iconName: Icons.Usdc,
   },
 ]
